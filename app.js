30f7b610f230a8c52b42e3a30d53f288868aa1a1
theeatch <dhruvnegiclub03@gmail.com>
2022-11-21 20:08:47 +0530

    first commit on files

---
+++ app.js
@@ -0,0 +1,57 @@
+const playTo = document.querySelector('#playto');
+const p1But = document.querySelector('#p1Button');
+const p2But = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#resetButton');
+
+let isGameOver = false;
+let winningScore = parseInt(playTo.value);
+let p1Score = 0;
+let p2Score = 0;
+
+p1But.addEventListener('click', function () {
+    if (!isGameOver) {
+        p1Score += 1
+        if (p1Score === winningScore) {
+            p1Display.style.color="green";
+            p2Display.style.color="red";
+            isGameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+
+
+}
+)
+p2But.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Score += 1
+        if (p2Score === winningScore) {
+            p1Display.style.color="red";
+            p2Display.style.color="green";
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+}
+)
+
+playTo.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset);
+
+
+function reset(){
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.style.color="black";
+    p2Display.style.color="black";
+    p1Score = 0;
+    p2Score = 0;
+    isGameOver = false;
+    
+}
